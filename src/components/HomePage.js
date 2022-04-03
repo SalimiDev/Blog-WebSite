9b24edc794d017958b6bd9c041f251c0ec975360
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-04-03 19:38:34 +0430

    feat:added google login to homepage component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,11 +2,18 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectSignedIn, setSignedIn, setUserData } from '../features/userSlice';
-// style
-import '../styling/home.css';
+//style
+import '../styles/home.css';
 
 const Homepage = () => {
     const isSignedIn = useSelector(selectSignedIn);
+
+    const dispatch = useDispatch();
+    const login = response => {
+        console.log(response);
+        dispatch(setSignedIn(true));
+        dispatch(setUserData(response.profileObj));
+    };
 
     return (
         <div className='home__page' style={{ display: isSignedIn ? 'none' : '' }}>
@@ -18,7 +25,18 @@ const Homepage = () => {
                         We provide high quality online resource for reading blogs. Just sign up and start reading some
                         quality blogs.
                     </p>
-                    <button className='login__button'>Login with Google</button>
+                    <GoogleLogin
+                        clientId='1084334404054-jch76dh3eqbj787ostatku2t93h61muv.apps.googleusercontent.com'
+                        render={renderProps => (
+                            <button onClick={renderProps.onClick} disabled={renderProps.disabled} className='login__button'>
+                                Login with Google
+                            </button>
+                        )}
+                        onSuccess={login}
+                        onFailure={login}
+                        isSignedIn={true}
+                        cookiePolicy={'single_host_origin'}
+                    />
                 </div>
             ) : (
                 ''
